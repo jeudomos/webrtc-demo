2c0a8211ba01a316d5fa3d8dcb4cbddc5127208a
Captain Eudomos <jeudomos@gmail.com>
2019-07-17 02:03:55 +0300

    sen data is added

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ let id;
 let clients = {};
 let localStream;
 const clientsContainer = document.querySelector('#clients');
+const chat = document.querySelector('#chat');
 
 var pcConfig = {
     'iceServers': [{
@@ -21,6 +22,7 @@ socket
             console_log('yes clients ok');
             if (!client.comm) {
                 client.comm = new RTCPeerConnection(pcConfig);
+                client.sendChannel = client.comm.createDataChannel('sendDataChannel');
                 client.comm.addEventListener('icecandidate', event => {
                     if (event.candidate) {
                         console_log('ice candiate sending rtc');
@@ -31,12 +33,17 @@ socket
                         });
                     }
                 })
+                client.comm.addEventListener('datachannel', event => {
+                    event.channel.addEventListener('message', event => {
+                        console.log(chat);
+                        sendChat(client.id, event.data);
+                    })
+                });
                 client.comm.addEventListener('addstream', event => {
                     console_log('++++++++++stream added++++++++++')
                     const video = document.querySelector(`video[client-id="${message.target}"]`);
                     video.className = "video-active";
                     console_log(event.stream);
-                    video.muted = true;
                     video.srcObject = event.stream;
                     if (!video.onloadedmetadata) {
                         video.onloadedmetadata = () => {
@@ -112,10 +119,22 @@ function refreshClients(newClients) {
 function createClient(id) {
     let li = document.createElement('li');
     li.setAttribute('id', id)
-    li.innerHTML = `<div><span>${id}</span><button client-id="${id}">Call</button></div><video client-id="${id}"></video>`
+    li.innerHTML = `
+    <div><span>${id}</span><button client-id="${id}">Call</button></div>
+    <div><textarea client-id="${id}" cols="30" rows="10"></textarea><button client-id="s-${id}">Send</button></div>
+    <video client-id="${id}"></video>`
     clientsContainer.appendChild(li);
     const btn = document.querySelector(`button[client-id="${id}"]`);
     const client = { id };
+    const btnSend = document.querySelector(`button[client-id="s-${id}"]`);
+    btnSend.addEventListener('click', event => {
+        event.preventDefault();
+        const txt = document.querySelector(`textarea[client-id="${client.id}"]`);
+        console_log('send value', txt.value);
+        client.sendChannel.send(txt.value);
+        sendChat("YOU", txt.value);
+        txt.value = "";
+    })
     btn.addEventListener('click', event => {
         console_log('calling')
         event.preventDefault();
@@ -136,7 +155,6 @@ function createClient(id) {
                 console_log('event listener :addstream');
                 console_log(event);
                 const video = document.querySelector(`video[client-id="${id}"]`);
-                video.muted = true;
                 video.srcObject = event.stream;
                 if (!video.onloadedmetadata) {
                     video.onloadedmetadata = () => {
@@ -146,7 +164,14 @@ function createClient(id) {
                     };
                 }
             })
-        }
+            client.sendChannel = client.comm.createDataChannel("sendData");
+            client.comm.addEventListener('datachannel', event => {
+                event.channel.addEventListener('message', event => {
+                    sendChat(client.id, event.data);
+                });
+            })
+        } else
+            console_log('channel already reserved')
         if (localStream) {
             createOffer(client);
         }
@@ -161,7 +186,7 @@ startCam.addEventListener('click', event => {
         return;
     navigator.mediaDevices.getUserMedia({
         video: true,
-        audio: false
+        audio: true
     }).then(stream => {
         const vid = document.querySelector('#localVideo');
         if (!vid.onloadedmetadata) {
@@ -203,7 +228,14 @@ function console_log(...args) {
     const now = new Date().getTime();// (window.performance.now() / 1000).toFixed(3);
     console.log(now, ...args);
     const msg = now;
+    /*
     const p = document.createElement('p');
     p.innerText = msg + args.join(' ');
     messages.appendChild(p);
+    */
+}
+function sendChat(id, msg) {
+    if (chat.value.length > 0)
+        chat.value += "\n";
+    chat.value += id + ">>" + msg;
 }
