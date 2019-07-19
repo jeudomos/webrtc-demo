02f50563c593eba10044629551b728e67d838877
Captain Eudomos <jeudomos@gmail.com>
2019-07-19 15:08:42 +0300

    file transfer added

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,94 @@ var pcConfig = {
     }]
 };
 
+function handleBinaryChannel(client, event) {
+    client.inpBuffer=[];
+    client.inpBufferLength=0;
+    event.channel.addEventListener('message',event=>{
+        console_log('DATA COMES');
+        console.dir(event);
+        client.inpBuffer.push(event.data);
+        client.inpBufferLength+=event.data.byteLength;
+        console_log(client.inpBufferLength,client.inpBuffer.length);
+        if ( client.inpBufferLength >= client.confirmFileInfo.size) {
+            console_log('YESSS COMPLET');
+            const img=document.querySelector('#remoteImg');
+            const blob=new Blob(client.inpBuffer);
+            client.inpBuffer=[];
+            img.src=URL.createObjectURL(blob);
+            console_log(img.src)
+        }
+    })
+}
+
+function handleJsonChannel(client, event) {
+    client.jsonChannel = event.channel;
+    const fnSend = client.jsonChannel.send.bind(client.jsonChannel);
+    client.jsonChannel.send = (data) => {
+        console.log('RESULT is');
+        console.log(data);
+        fnSend(JSON.stringify(data));
+    }
+    event.channel.addEventListener('message', event => {
+        handleJsonMessageEvent(client,event);
+    })
+}
+
+function handleJsonMessageEvent(client, event) {
+    console.log(event);
+    const msg = JSON.parse(event.data);
+    switch (msg.operation) {
+        case "chat":
+            sendChat(client.id, msg.message);
+            break;
+        case "upload":
+            if (confirm(`${client.id} wants to send you ${msg.fileInfo.name}. Would you like to accept?`)) {
+                client.jsonChannel.send({ operation: "upload_response", result: true });
+                client.confirmFileInfo=msg.fileInfo;
+            } else
+                client.jsonChannel.send({ operation: "uploa_responsed", result: false });
+            break;
+        case "upload_response":
+            if (msg.result) {
+                client.dataChannel=client.comm.createDataChannel("file-upload");
+                client.inpOffset=0;
+                client.cnt=0;
+                console_log('SEND FILE');
+                console.log(client);
+                sendFile(client);
+            } else
+                alert('decliened')
+    }
+}
+
+function sendFile(client) {
+    console_log('SENDING FILE');
+    console.log(client);
+    if(!client.reader.onload) {
+        client.reader.onload=(event)=>{
+            console.log('EVENT');
+            console.log(event);
+            console.log(client);
+            client.inpOffset+=event.target.result.byteLength;
+            console.log(client);
+            client.dataChannel.send(event.target.result);
+            console.log(client.inpOffset,client.inpFile.files[0].size);
+            if (client.inpOffset < client.inpFile.files[0].size)
+                sendFile(client);
+            else
+            {
+                client.dataChannel.close();
+                client.dataChannel=null;
+            }
+        }
+    }
+    if(client.inpOffset < client.inpFile.files[0].size) {
+        const slice=client.inpFile.files[0].slice(client.inpOffset,client.inpOffset+32*1024);
+        client.reader.readAsArrayBuffer(slice);
+    }
+}
+
+
 socket
     .on('id', ({ id }) => setId(id))
     .on('clients', ({ clients }) => refreshClients(clients))
@@ -22,7 +110,6 @@ socket
             console_log('yes clients ok');
             if (!client.comm) {
                 client.comm = new RTCPeerConnection(pcConfig);
-                client.sendChannel = client.comm.createDataChannel('sendDataChannel');
                 client.comm.addEventListener('icecandidate', event => {
                     if (event.candidate) {
                         console_log('ice candiate sending rtc');
@@ -34,10 +121,12 @@ socket
                     }
                 })
                 client.comm.addEventListener('datachannel', event => {
-                    event.channel.addEventListener('message', event => {
-                        console.log(chat);
-                        sendChat(client.id, event.data);
-                    })
+                    console_log('DATA CHANNEL')
+                    if (event.channel.label === "json") {
+                        handleJsonChannel(client, event);
+                    } else {
+                        handleBinaryChannel(client, event);
+                    }
                 });
                 client.comm.addEventListener('addstream', event => {
                     console_log('++++++++++stream added++++++++++')
@@ -48,11 +137,11 @@ socket
                     if (!video.onloadedmetadata) {
                         video.onloadedmetadata = () => {
                             console_log('>>>>>>>>>>>>>>>>>>>>loaded meta');
-                            console.dir(video);
+                            console.log(video);
                             video.play().then(() => console_log("VIDEO PLAY START")).catch(err => console_log("ERROR VIDEO", err))
                         };
                     }
-                    console.dir(video);
+                    console.log(video);
                 })
             }
             switch (message.type) {
@@ -121,17 +210,36 @@ function createClient(id) {
     li.setAttribute('id', id)
     li.innerHTML = `
     <div><span>${id}</span><button client-id="${id}">Call</button></div>
+    <div><input client-id="${id}" type="file"><button client-id="f-${id}">Send</button></div>
     <div><textarea client-id="${id}" cols="30" rows="10"></textarea><button client-id="s-${id}">Send</button></div>
     <video client-id="${id}"></video>`
     clientsContainer.appendChild(li);
     const btn = document.querySelector(`button[client-id="${id}"]`);
     const client = { id };
+    client.inpFile = document.querySelector(`input[client-id="${id}"]`);
+    client.reader=new FileReader();
+    const btnFile = document.querySelector(`button[client-id="f-${id}"]`);
+    btnFile.addEventListener('click', event => {
+        file = client.inpFile.files[0];
+        client.fileInfo = {
+            name: file.name,
+            size: file.size,
+            type: file.type
+        };
+        client.jsonChannel.send({
+            operation: "upload",
+            fileInfo: client.fileInfo
+        });
+    });
     const btnSend = document.querySelector(`button[client-id="s-${id}"]`);
     btnSend.addEventListener('click', event => {
         event.preventDefault();
         const txt = document.querySelector(`textarea[client-id="${client.id}"]`);
         console_log('send value', txt.value);
-        client.sendChannel.send(txt.value);
+        client.jsonChannel.send({
+            operation: "chat",
+            message: txt.value
+        });
         sendChat("YOU", txt.value);
         txt.value = "";
     })
@@ -159,22 +267,28 @@ function createClient(id) {
                 if (!video.onloadedmetadata) {
                     video.onloadedmetadata = () => {
                         console_log('>>>>>>>>>>>>>>>>>>>>loaded meta');
-                        console.dir(video);
+                        console.log(video);
                         video.play().then(() => console_log("VIDEO PLAY START")).catch(err => console_log("ERROR VIDEO", err))
                     };
                 }
             })
-            client.sendChannel = client.comm.createDataChannel("sendData");
+            client.jsonChannel = client.comm.createDataChannel("json");
+            client.jsonChannel.addEventListener('message',event=>handleJsonMessageEvent(client,event))
+            const send = client.jsonChannel.send.bind(client.jsonChannel);
+            console.log('------------ JSON CHANNEL ---------------')
+            console.log(client.jsonChannel);
+            client.jsonChannel.send = (data) => {
+                send(JSON.stringify(data));
+            }
             client.comm.addEventListener('datachannel', event => {
-                event.channel.addEventListener('message', event => {
-                    sendChat(client.id, event.data);
-                });
+                if (event.channel.label === "json") {
+                    handleJsonChannel(client, event);
+                } else
+                    handleBinaryChannel(client, event);
             })
         } else
             console_log('channel already reserved')
-        if (localStream) {
-            createOffer(client);
-        }
+        createOffer(client);
     })
     clients[id] = client;
 }
@@ -203,10 +317,8 @@ startCam.addEventListener('click', event => {
 
 function createOffer(client) {
     console_log('adding stream');
-    console_log(client);
-    client.comm.addStream(localStream);
-    console_log('creating offer')
-    client.comm.createOffer({ offerToReceiveVideo: 1 }).then(description => {
+    //client.comm.addStream(localStream);
+    client.comm.createOffer().then(description => {
         try {
             console_log('created offer setting local and sending offer rtc');
             console_log(description);
